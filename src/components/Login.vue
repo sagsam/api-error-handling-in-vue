32a2495e18d88573da2167b59d309a92b4f22428
sagsam <saagrsaaml@gmail.com>
2018-09-16 01:46:15 +0545

    create Login component and use it

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="login">
+    <form method="post" @submit.prevent="onSubmit">
+
+      <label for="username">        
+        Username: 
+      </label>
+      <input type="email" name="username">
+
+      <br>
+
+      <label for="password">        
+        Password: 
+      </label>
+      <input type="password" name="password">
+
+      <br>
+
+      <input type="submit" name="login" value="Login">
+      
+    </form>  
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  methods: {
+    onSubmit(){
+      
+    }
+  }
+}
+</script>
+
+<style>
+</style>
