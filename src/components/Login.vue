804a7e487e7cae0f54ad2a6bc95100534f518ad5
sagsam <saagrsaaml@gmail.com>
2018-09-20 20:08:22 +0545

    style and set error message

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,29 @@
 <template>
   <div id="login">    
     <form method="post" @submit.prevent="onSubmit" @keydown="form.errors.clear()">
-      <span v-if="form.errors.has('non_field_errors')" v-text="form.errors.get('non_field_errors')"></span>
+      <span class="error__message" v-if="form.errors.has('non_field_errors')" v-text="form.errors.get('non_field_errors')"></span>
       <br>
 
       <label for="username">        
         Username: 
       </label>
-      <input type="email" name="username" v-model="form.username" v-error>
-      <br>
-      <span v-if="form.errors.has('username')" v-text="form.errors.get('username')"></span>
-      <br>
+      <p>
+        <input type="email" name="username" v-model="form.username" v-error="form.errors.get('username')">
+      </p>
+      
+      
+      <!-- <span v-if="form.errors.has('username')" v-text="form.errors.get('username')"></span>
+      <br> -->
 
       <label for="password">        
         Password: 
       </label>
-      <input type="password" name="password" v-model="form.password">
-      <br>
-      <span v-if="form.errors.has('password')" v-text="form.errors.get('password')"></span>
-      <br>
+      <p>
+        <input type="password" name="password" v-model="form.password" v-error="form.errors.get('password')">
+      </p>
+      
+      <!-- <span v-if="form.errors.has('password')" v-text="form.errors.get('password')"></span>
+      <br> -->
 
       <input type="submit" name="login" value="Login">
 
@@ -53,4 +58,33 @@ export default {
 </script>
 
 <style>
+input[type="password"],
+input[type="email"] {
+  padding: 10px;
+  border: solid 2px #c9c9c9;
+  font-size: 18px;
+  border-radius: 4px;
+  outline: 0;
+}
+
+input[type="password"].focus,
+input[type="password"]:focus,
+input[type="email"].focus,
+input[type="email"]:focus {
+  outline: 0;
+}
+
+input[type="password"].error,
+input[type="email"].error{
+  border: solid 2px #F84036;
+  outline: 0;
+  color:#F84036;
+}
+
+.error__message{
+  padding-top:4px;
+  font-size:90%;
+  color:#F84036;
+}
+
 </style>
--- src/directives/error.js
+++ src/directives/error.js
@@ -1,7 +1,28 @@
 export default {
     bind(el, binding, vnode) {
-        console.log(el)
-        console.log(binding)
-        console.log(vnode)
+        el.verror = document.createElement("DIV")
+        el.verror.className = 'error__message'
+        // clear the error class of input
+        el.addEventListener('input', () => {    
+            const errorList = document.querySelectorAll('.error')
+            for ( let i = 0; i < errorList.length; i += 1 ) {
+                errorList[i].className = ""
+            }               
+        })        
+    },
+    componentUpdated(el, binding, vnode) {
+        // upadate error message
+        if(binding.value) {
+            el.verror.innerHTML = binding.value
+        }        
+        // check if verror is present to remove
+        if(el.verror && el.verror.parentNode !== null ){
+            el.verror.parentNode.removeChild( el.verror )
+        }
+        // check if verror is present to add
+        if(el.verror && binding.value) {
+            el.className = "error";          
+            el.parentNode.appendChild( el.verror )           
+        }
     }
 }
